6691f994d09f30b96afafd1d2a1b68303889a1b6
paladiny <paladinwyh@gmail.com>
2019-09-25 17:26:47 +0800

    Update openport.sh

--- openport.sh
+++ openport.sh
@@ -12,27 +12,27 @@ function openPort(){
 }
 
 function echoInfo(){
-	echo -e "\e[1;36m$1\e[0m\n"
+	echo -e "\e[1;36m$1\e[0m"
 }
 
 function echoResult(){
-	echo -e "\e[1;32m$1\e[0m\n"
+	echo -e "\e[1;32m$1\e[0m"
 }
 
 function echoError(){
-	echo -e "\e[1;35m$1\e[0m\n"
+	echo -e "\e[1;35m$1\e[0m"
 }
 
 
 
 openPort
-echoResult '已临时放开所有端口' 
-echoInfo '添加启动项，每次服务器重启后会自动放开所有端口'
+echoResult "已临时放开所有端口\n" 
+echoInfo '准备添加启动项，每次服务器重启后会自动放开所有端口'
 
 echoInfo '检查文件 /lib/systemd/system/rc.local.service 内容中是否有[Install]段'
 grep '[Install]' /lib/systemd/system/rc.local.service > /dev/null
 if [ $? -eq 0 ]; then
-	echoResult '检查结果：[Install]段已存在'
+	echoResult "检查结果：[Install]段已存在\n"
 else
 	echoError '检查结果：[Install]段不存在'
 	echoInfo '添加[Install]段'
@@ -40,40 +40,40 @@ else
 	echo '[Install]'>> /lib/systemd/system/rc.local.service
 	echo 'WantedBy=multi-user.target'>> /lib/systemd/system/rc.local.service
 	echo 'Alias=rc-local.service' >> /lib/systemd/system/rc.local.service
-	echoResult '添加完成'
+	echoResult "添加完成\n"
 fi
 
 echoInfo '检查文件 /etc/rc.local 是否存在'
 if [ -f "/etc/rc.local" ]; then
-	echoResult '检查结果：/etc/rc.local 文件存在'
+	echoResult "检查结果：/etc/rc.local 文件存在\n"
 	echoInfo '检查重启是否自动放开所有端口'
 	grep 'iptables -P' /etc/rc.local > /dev/null
 	if [ $? -eq 0 ]; then
-		echoResult '检查结果：自动放开所有端口'
+		echoResult "检查结果：自动放开所有端口\n"
 	else
 		echoError '检查结果：无法自动放开所有端口'
 		echoInfo '正在添加脚本到rc.local'
 		echo -e "$a\n$b\n$c\n$d\n" >>/etc/rc.local
-		echoResult '添加完成'
+		echoResult "添加完成\n"
 	fi
 
 else
 	echoError '文件不存在'
 	echoInfo '创建文件 /etc/rc.local'
 	echo -e "$a\n$b\n$c\n$d\nexit 0\n" >>/etc/rc.local
-	echoResult '创建完成'
+	echoResult "创建完成\n"
 
 	chmod 755 /etc/rc.local
 fi
 
-echoInfo '检查软链接是否存在'
+echoInfo '检查/etc/systemd/system/目录下是否有/lib/systemd/system/rc.local.service的软链接'
 if [ -f "/etc/systemd/system/rc.local.service" ];then
-	echoResult '软链接存在'
+	echoResult "软链接存在\n"
 	else
 		echoError '软链接不存在'
 		echoInfo '创建软链接'
 		ln -s /lib/systemd/system/rc.local.service /etc/systemd/system/
-		echoResult '创建完成'
+		echoResult "创建完成\n"
 fi
 
 echoInfo '脚本执行完毕'
