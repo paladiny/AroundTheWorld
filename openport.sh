f50f92e7c3e8410a310eb45261f654bb62b898cf
paladiny <paladinwyh@gmail.com>
2019-09-25 17:37:36 +0800

    Update openport.sh

--- openport.sh
+++ openport.sh
@@ -30,7 +30,7 @@ echoResult "已临时放开所有端口\n"
 echoInfo '准备添加启动项，每次服务器重启后会自动放开所有端口'
 
 echoInfo '检查文件 /lib/systemd/system/rc.local.service 内容中是否有[Install]段'
-grep '[Install]' /lib/systemd/system/rc.local.service > /dev/null
+grep 'Install' /lib/systemd/system/rc.local.service > /dev/null
 if [ $? -eq 0 ]; then
 	echoResult "检查结果：[Install]段已存在\n"
 else
@@ -45,23 +45,21 @@ fi
 
 echoInfo '检查文件 /etc/rc.local 是否存在'
 if [ -f "/etc/rc.local" ]; then
-	echoResult "检查结果：/etc/rc.local 文件存在\n"
+	echoResult "检查结果：文件存在\n"
 	echoInfo '检查重启是否自动放开所有端口'
 	grep 'iptables -P' /etc/rc.local > /dev/null
 	if [ $? -eq 0 ]; then
 		echoResult "检查结果：自动放开所有端口\n"
 	else
 		echoError '检查结果：无法自动放开所有端口'
-		echoInfo '正在添加脚本到rc.local'
+		echoInfo '添加脚本到rc.local'
 		echo -e "$a\n$b\n$c\n$d\n" >>/etc/rc.local
-		echoResult "添加完成\n"
 	fi
 
 else
 	echoError '文件不存在'
 	echoInfo '创建文件 /etc/rc.local'
 	echo -e "$a\n$b\n$c\n$d\nexit 0\n" >>/etc/rc.local
-	echoResult "创建完成\n"
 
 	chmod 755 /etc/rc.local
 fi
@@ -73,7 +71,6 @@ if [ -f "/etc/systemd/system/rc.local.service" ];then
 		echoError '软链接不存在'
 		echoInfo '创建软链接'
 		ln -s /lib/systemd/system/rc.local.service /etc/systemd/system/
-		echoResult "创建完成\n"
 fi
 
 echoInfo '脚本执行完毕'
